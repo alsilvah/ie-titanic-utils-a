5156c7d2e0ada1e3f534b4dd85708ba7ade65e5a
Alejandro Francisco Silva Hernandez <alejandro.silva.h@student.ie.edu>
2021-06-25 11:19:41 +0200

    add app and new output

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from ie_utils import tokenize
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"message":"Hello World!","version":"0.0.1",
+    
+
+@app.route("/tokenize")
+def do_tokenize():
+    print(request.args)
+    sentence=request.args["sentence"]
+    lower=bool(request.args.get("lower",False))
+    return str(tokenize(sentence))
